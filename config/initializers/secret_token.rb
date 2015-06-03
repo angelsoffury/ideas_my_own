9d57e004c38177cb8bb4c3d88b2eccd0bc2144a2
angelsoffury <ahalyadas01@gmail.com>
2015-06-03 14:31:52 +0530

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdeasMyOwn::Application.config.secret_key_base = '0f66375df3c40d15937e12a7d0c325ffddd529692990b909394df6720b6bbc4ea71b51c7028db0a419fdb2a17bb25ea78c2001e39e676c7cc2d301abdc92f704'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IdeasMyOwn::Application.config.session_store :cookie_store, key: '_ideas_my_own_session'
